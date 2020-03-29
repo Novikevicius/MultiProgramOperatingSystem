146bc563fcdfcc0dd3be7bf9b01366ef4e880176
Novikevicius <edvardas.novikevicius@gmail.com>
2020-03-29 16:30:14 +0300

    added RM registers

--- RealMachine/RM.java
+++ RealMachine/RM.java
@@ -1,5 +1,18 @@
 package MultiProgramOperatingSystem.RealMachine;
 
 public class RM {
-        
+    private byte MODE;
+    private int PTR;
+    private int IC;
+    private byte PI;
+    private byte SI;
+    private int R1;
+    private int R2;
+    private int R3;
+    private byte CMP;
+    private byte IO;
+    private int TI;
+    private byte CH1;
+    private byte CH2;
+    private byte CH3;
 }
