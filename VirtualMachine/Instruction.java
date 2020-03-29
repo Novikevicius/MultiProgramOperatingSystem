97370829bb8b454d2a06c82900fe9ca75a9f4bb0
Novikevicius <edvardas.novikevicius@gmail.com>
2020-03-29 19:14:19 +0300

    added Instruction enum with ADD, SUB, MUL & HALT

--- VirtualMachine/Instruction.java
+++ VirtualMachine/Instruction.java
@@ -6,15 +6,31 @@ package MultiProgramOperatingSystem.VirtualMachine;
 public enum Instruction {
     ADD(0x01),
     SUB(0x02),
-    MUL(0x03);
+    MUL(0x03),
+    HALT(0x04);
     private int opcode;
-    
+    private int args;
     private Instruction(int opcode)
     {
+        this(opcode, 0);
+    }
+    private Instruction(int opcode, int argCount)
+    {
         this.opcode = opcode;
+        this.args = argCount;
     }
     public int getOpcode()
     {
         return opcode;
+    }
+    public int getArgCount()
+    {
+        return args;
+    }
+    public static int getOpcodeByName(String name){
+       return Instruction.valueOf(name).getOpcode();
+    }
+    public static Instruction getInstructionByName(String name){
+       return Instruction.valueOf(name);
     }
 }
