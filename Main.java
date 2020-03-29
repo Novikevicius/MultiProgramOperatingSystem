f06a5db807f4d1dfec4047bd9ee3faa0607aa478
Novikevicius <edvardas.novikevicius@gmail.com>
2020-03-29 20:01:09 +0300

    added debug flag

--- Main.java
+++ Main.java
@@ -3,11 +3,14 @@ package MultiProgramOperatingSystem;
 import MultiProgramOperatingSystem.VirtualMachine.VM;
 
 public class Main {
+    public static final boolean DEBUG = false;
     public static void main(String[] args) {
         VM vm = new VM();
         try {
             vm.loadProgram("MultiProgramOperatingSystem/Program.txt");
+            vm.printRegisters();
             vm.runProgram();
+            vm.printRegisters();
         } catch (Exception e) {
             e.printStackTrace();
         }
