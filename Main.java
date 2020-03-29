b095342e6a7c96e7650476c85842024cd1aecb12
Novikevicius <edvardas.novikevicius@gmail.com>
2020-03-29 17:53:48 +0300

    added example program

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 package MultiProgramOperatingSystem;
 
+import MultiProgramOperatingSystem.VirtualMachine.VM;
+
 public class Main {
     public static void main(String[] args) {
+        VM vm = new VM();
+        try {
+            vm.runProgram("MultiProgramOperatingSystem/Program.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("Man reik daryt Lygiagrecius!");
     }
 }
