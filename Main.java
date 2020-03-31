f5721c896b506154c66b8e881fc70e4b589ebf56
Novikevicius <edvardas.novikevicius@gmail.com>
2020-03-31 20:25:43 +0300

    init page table for VM and added simple example

--- Main.java
+++ Main.java
@@ -4,14 +4,14 @@ import MultiProgramOperatingSystem.RealMachine.*;
 import MultiProgramOperatingSystem.VirtualMachine.*;
 
 public class Main {
-    public static final boolean DEBUG = false;
+    public static final boolean DEBUG = true;
   
     public static void main(String[] args) {
         RM rm = new RM();
-        //VM vm = new VM(rm);
+        VM vm = new VM(rm);
         rm.setPTR(1023, 0);
         for(int i = 0; i < RM.ENTRIES_PER_PAGE_TABLE; i++){
-            rm.setPTE(i, RM.ENTRIES_PER_PAGE_TABLE-i-1);
+            rm.setPTE(i, i);
         }
         rm.printPageTable();
         rm.setWord(15, 5, 10);
@@ -19,7 +19,7 @@ public class Main {
         System.out.println(rm.virtualToRealAddress(15, 5));
         System.out.println(rm.getWord(15, 5));
         
-        /*
+        
         try {
             vm.loadProgram("MultiProgramOperatingSystem/Program.txt");
             vm.printRegisters();
@@ -27,7 +27,7 @@ public class Main {
             vm.printRegisters();
         } catch (Exception e) {
             e.printStackTrace();
-        }*/
+        }
         System.out.println("Man reik daryt Lygiagrecius!");
     }
 }
