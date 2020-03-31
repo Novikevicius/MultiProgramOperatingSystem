0074053fd69b95de21b6172adffccfa7ecd9e45d
Novikevicius <edvardas.novikevicius@gmail.com>
2020-03-31 19:57:28 +0300

    implemented page table

--- Main.java
+++ Main.java
@@ -8,7 +8,18 @@ public class Main {
   
     public static void main(String[] args) {
         RM rm = new RM();
-        VM vm = new VM();
+        //VM vm = new VM(rm);
+        rm.setPTR(1023, 0);
+        for(int i = 0; i < RM.ENTRIES_PER_PAGE_TABLE; i++){
+            rm.setPTE(i, RM.ENTRIES_PER_PAGE_TABLE-i-1);
+        }
+        rm.printPageTable();
+        rm.setWord(15, 5, 10);
+        System.out.println(rm.getPageTableAddress());
+        System.out.println(rm.virtualToRealAddress(15, 5));
+        System.out.println(rm.getWord(15, 5));
+        
+        /*
         try {
             vm.loadProgram("MultiProgramOperatingSystem/Program.txt");
             vm.printRegisters();
@@ -16,7 +27,7 @@ public class Main {
             vm.printRegisters();
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
         System.out.println("Man reik daryt Lygiagrecius!");
     }
 }
--- RealMachine/RM.java
+++ RealMachine/RM.java
@@ -16,13 +16,78 @@ public class RM {
     private byte CH2;
     private byte CH3;
 
-    private static final int PAGE_SIZE = 16;
-    private static final int PAGE_COUNT_PER_VM = 16; // per one Virtual Machine
-    private static final int MAX_VM_COUNT = 64;
-    private static final int WORD_SIZE = 4;
+    public static final int PAGE_SIZE = 16;
+    public static final int PAGE_COUNT_PER_VM = 16; // per one Virtual Machine
+    public static final int MAX_VM_COUNT = 64;
+    public static final int WORD_SIZE = 4;
 
-    private byte[] MEMORY = new byte[PAGE_COUNT_PER_VM * PAGE_SIZE * MAX_VM_COUNT * WORD_SIZE];
+    public static final int ENTRIES_PER_PAGE_TABLE = PAGE_COUNT_PER_VM;
+    private int[] MEMORY = new int[PAGE_COUNT_PER_VM * PAGE_SIZE * MAX_VM_COUNT];
 
+    /**
+     * changes value in virtual memory which is mapped to memory in RAM
+     * @param page - page number in VM
+     * @param offset - offset in the page
+     * @param value - new value at specified memory location
+     */
+    public void setWord(int page, int offset, int value)
+    {
+        MEMORY[virtualToRealAddress(page, offset)] = value;
+    }
+    public int getWord(int page, int offset)
+    {
+        return MEMORY[virtualToRealAddress(page, offset)];
+    }
+    public int virtualToRealAddress(int page, int offset)
+    {
+        int tableAddress = getPageTableAddress();
+        return MEMORY[tableAddress + page] * PAGE_SIZE + offset;
+    }
+    public int getPageTableAddress()
+    {
+        // PTR:
+        // 00000000 (in hex)
+        // 0xxxxxyy
+        // xxxxx - page number in RAM where the Page Table is stored
+        // yy    - offset in page
+        int offset = PTR & 0xFF;
+        int page   = (PTR >> 8) & 0x03FF;  
+        return PAGE_SIZE * page + offset;
+    }
+    public void printPageTable()
+    {
+        int tableAddress = getPageTableAddress();
+        System.out.println( "---------Page table -----------------");
+        System.out.println( "PTR: " + getPTR());
+        for (int i = 0; i < ENTRIES_PER_PAGE_TABLE; i++) {
+            System.out.println( (tableAddress+i) + ": " + MEMORY[tableAddress+i]);
+        }
+        System.out.println( "-------------------------------------");
+    }
+    // set Page Table Entry
+    /***
+     * maps page in Virtual memory to Page in RAM
+     * @param pageInVM - page number in virtual memory, [0, 255]
+     * @param pageInRAM - page number in RAM, [0, 1023]
+     */
+    public void setPTE(int pageInVM, int pageInRAM)
+    {
+        if(pageInVM < 0 || pageInVM >= ENTRIES_PER_PAGE_TABLE || pageInRAM < 0 || pageInRAM > MAX_VM_COUNT * PAGE_COUNT_PER_VM)
+            return;
+        int table = getPageTableAddress();
+        MEMORY[table + pageInVM] = pageInRAM;
+    }
+    /***
+     * change Page Table Pointer to point to actual page table at specified location in RAM
+     * @param page   - page where the Page Table is stored in RAM
+     * @param offset - offset in the page for the Page Table pointer
+     */
+    public void setPTR(int page, int offset)
+    {
+        if( page < 0 || page > 0x03FF || offset < 0 || offset >= PAGE_SIZE)
+            return;
+        PTR = (page << 8) | offset;
+    }
     public void setZF(){
         CMP |= (1 << 6);
     }
@@ -142,4 +207,29 @@ public class RM {
     public void setMODE(byte MODE) {
         this.MODE = MODE;
     }
+    /* ---------Virtual Memory model-------*/
+    /*
+        0 1  2  3  4  5  6 7 8 9 A B C D E F|
+       -----------Data segment---------------
+       0            0-16                    |
+       1            16-32                   |
+       2            32-48                   |
+       3            48-64                   |
+       ------------Code segment--------------
+       4            64-80                   |
+       5            80-96                   |
+       6            96-112                  |
+       7            112-128                 |
+       8            128-144                 |
+       9            144-160                 |
+       A            160-176                 |
+       B            176-192                 |
+       -----------Page table ----------------
+       C            192-208                 |
+       -----------Shared memory--------------
+       D            208-224                 |
+       E            224-240                 |
+       F            240-256                 |
+       --------------------------------------
+     */
 }
