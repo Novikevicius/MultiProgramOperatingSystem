a2a87140e436ddc65922132e7633247b2fa550b7
zygis.rimgaila@gmail.com <zygis.rimgaila@gmail.com>
2020-04-02 19:49:51 +0300

    removed Printer try catch

--- RealMachine/Printer.java
+++ RealMachine/Printer.java
@@ -3,15 +3,11 @@ package MultiProgramOperatingSystem.RealMachine;
 public class Printer {
 
     public static void print(Object data) {
-        try{
-            System.out.print("Printer: ");
-            if (data instanceof char[]) {
-                System.out.println((char[]) data);
-            } else {
-                System.out.println(data);
-            }
-        } catch(IOException e) {
-            e.printStackTrace();
-        }    
+        System.out.print("Printer: ");
+        if (data instanceof char[]) {
+            System.out.println((char[]) data);
+        } else {
+            System.out.println(data);
+        } 
     }
 }
