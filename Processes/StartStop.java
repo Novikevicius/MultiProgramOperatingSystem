4ccccf18cd0f694784aae60d4dfa21b937041e03
Novikevicius <edvardas.novikevicius@gmail.com>
2020-05-25 16:10:59 +0300

    added StartStop ctor

--- Processes/StartStop.java
+++ Processes/StartStop.java
@@ -1,6 +1,12 @@
 package MultiProgramOperatingSystem.Processes;
 
+import MultiProgramOperatingSystem.MOS.Kernel;
+
 public class StartStop extends Process {
+    
+    public StartStop(Kernel k){
+        super(k);
+    }
     @Override
     public void run() {
         
