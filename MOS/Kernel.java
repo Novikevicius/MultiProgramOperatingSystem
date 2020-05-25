8e2f506efb5a0c13f0a995bc3efceb44fda5a7fd
Novikevicius <edvardas.novikevicius@gmail.com>
2020-05-25 16:00:17 +0300

    added Kernel process primitive prototypes

---
+++ MOS/Kernel.java
@@ -0,0 +1,22 @@
+package MultiProgramOperatingSystem.MOS;
+
+public class Kernel {
+    public void start(){
+
+    }
+    public void createProcess(){
+
+    }
+    public void destroyProcess(){
+
+    }
+    public void activateProcess(){
+
+    }
+    public void stopProcess(){
+
+    }
+    public void changePriority(){
+
+    }
+}
