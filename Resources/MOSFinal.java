5ac0551168f957e1d4691c217a2b7e38c446acd3
Novikevicius <edvardas.novikevicius@gmail.com>
2020-05-25 16:51:35 +0300

    created Resource basic methods

--- Resources/MOSFinal.java
+++ Resources/MOSFinal.java
@@ -1,5 +1,8 @@
 package MultiProgramOperatingSystem.Resources;
 
 public class MOSFinal extends Resource {
-    
+    public MOSFinal(Process creator)
+    {
+        super("MOSFinal", creator);
+    }
 }
--- Resources/Resource.java
+++ Resources/Resource.java
@@ -1,5 +1,26 @@
 package MultiProgramOperatingSystem.Resources;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public abstract class Resource {
-    
+    private static int IDs = 0;
+    protected int id;
+    protected String name;
+    protected Process creator;
+    protected ArrayList<Resource> resourceElements = new ArrayList<>();
+    protected HashMap<Process, Integer> waitingProcesses = new HashMap<>(); // Process & how many resources it needs
+
+    protected Resource(String name, Process creator)
+    {
+        this.id = IDs++;
+        this.name = name;
+        this.creator = creator;
+    }
+    public void freeResource(Resource element){
+        this.resourceElements.add(element);
+    }
+    public void requestResource(Process process, int count){
+        waitingProcesses.put(process, count);
+    }
 }
