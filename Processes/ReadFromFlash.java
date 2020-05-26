e8d7b5844041c61c47d0d57dce13465ae68b4f76
Novikevicius <edvardas.novikevicius@gmail.com>
2020-05-26 19:52:40 +0300

    added ReadFromFlash process

---
+++ Processes/ReadFromFlash.java
@@ -0,0 +1,36 @@
+package MultiProgramOperatingSystem.Processes;
+
+import MultiProgramOperatingSystem.RealMachine.FlashMemory;
+import MultiProgramOperatingSystem.Resources.*;
+
+public class ReadFromFlash extends Process {
+    
+    public ReadFromFlash(Process parent){
+        super(parent, "ReadFromFlash", 15);
+    }
+    @Override
+    public void execute() {
+        switch(counter)
+        {
+            case 0:
+            kernel.requestResource(this, new InputStreamResource());
+            break;
+
+            case 1:
+            kernel.requestResource(this, new SupervisorMemoryResource());
+            break;
+
+            case 2:
+            FlashMemory.readFromFlashToSupervisorMemory();
+            break;
+
+            case 3:
+            //kernel.freeResource(new TaskInMemoryResource);
+            counter = 0;
+            break;
+
+            default:
+            System.out.println("Unrecognized step for " + this + ": " + counter);
+        }
+    }
+}
