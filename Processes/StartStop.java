5268b1f247ba53bea98cbffe159481f25b9513fb
Novikevicius <edvardas.novikevicius@gmail.com>
2020-05-26 20:01:06 +0300

    added StartStop process

--- Processes/StartStop.java
+++ Processes/StartStop.java
@@ -1,5 +1,7 @@
 package MultiProgramOperatingSystem.Processes;
 
+import MultiProgramOperatingSystem.Resources.*;
+
 public class StartStop extends Process {
     
     public StartStop(){
@@ -7,6 +9,24 @@ public class StartStop extends Process {
     }
     @Override
     public void execute() {
-        
+        switch(counter){
+            case 0:
+            kernel.createResource(new MOSFinal(this));
+            kernel.createResource(new InputStreamResource(this));
+            kernel.createResource(new SupervisorMemoryResource(this));
+            kernel.createResource(new MemoryResource(this));
+            break;
+            
+            case 1:
+            kernel.createProcess(new ReadFromFlash(this));
+            break;
+
+            case 2:
+            kernel.requestResource(this, new MOSFinal(this));
+            break;
+        default:
+            System.out.println("Unrecognized step for " + this + ": " + counter);
+            break;
+        }
     }
 }
