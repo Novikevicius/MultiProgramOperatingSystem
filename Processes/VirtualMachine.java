3a2ce42a561189271c4ed6e07df5458afe7a5a4e
Novikevicius <edvardas.novikevicius@gmail.com>
2020-05-28 22:01:34 +0300

    vm process can now remember ptr

--- Processes/VirtualMachine.java
+++ Processes/VirtualMachine.java
@@ -5,7 +5,8 @@ import MultiProgramOperatingSystem.VirtualMachine.VM;
 public class VirtualMachine extends Process {
 
     private VM vm;
-
+    private int ptr;
+    private int old_ptr;
     public VirtualMachine(Process parent) {
         super(parent, "VirtualMachine", 5);
         vm = new VM(kernel.getRM(), this);
@@ -16,6 +17,9 @@ public class VirtualMachine extends Process {
         switch (counter) {
             case 0:
                 kernel.getRM().setMODE((byte) 0);
+                old_ptr = kernel.getRM().getPTR();
+                vm.loadValues();
+                kernel.getRM().setPTR(ptr);
                 break;
             case 1:
                 try {
@@ -23,11 +27,20 @@ public class VirtualMachine extends Process {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                vm.saveValues();
+                kernel.getRM().setPTR(old_ptr);
                 counter = -1;
             break;
 
             default:
             System.out.println("Unrecognized step for " + this + ": " + counter);
         }
+    }
+    public void setPTR( int ptr ){
+        this.ptr = ptr;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + " " + id;
     }
 }
