c9f7e7fa3254681237a62ac609e98e9a59dd7cde
Novikevicius <edvardas.novikevicius@gmail.com>
2020-05-28 21:59:57 +0300

    added additional debug setting

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import MultiProgramOperatingSystem.MOS.Kernel;
 
 public class Main {
     public static final boolean DEBUG = false;
+    public static final boolean DEBUG_VM = true;
   
     public static void main(String[] args) {
         Kernel kernel = Kernel.getInstance();
