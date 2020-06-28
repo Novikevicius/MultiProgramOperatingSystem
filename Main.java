7956d1116d00c925544a43483aa99799df70712b
Novikevicius <edvardas.novikevicius@gmail.com>
2020-06-28 13:29:36 +0300

    added VERBOSE debug parameter

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ package MultiProgramOperatingSystem;
 import MultiProgramOperatingSystem.MOS.Kernel;
 
 public class Main {
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
+    public static final int VERBOSE = 0;
     public static final boolean DEBUG_VM = true;
   
     public static void main(String[] args) {
